e2a046a916a93c9067fe266e988e27904db29c7e
OlhaLu <olhalutsyshyna@gmail.com>
2020-07-02 15:54:36 +0300

    added server configs, created table sqlite

---
+++ server/server.js
@@ -0,0 +1,79 @@
+﻿const express = require("express");
+const cors = require("cors");
+const sqlite3 = require("sqlite3").verbose();
+// let db = new sqlite3.Database("../");
+
+const app = express();
+app.use(cors());
+
+app.get("/statistic/:userId", (req, res) => {
+  const [today] = new Date().toISOString().split("T");
+  const { from = "0000-00-00", to = today } = req.query;
+  const { userId } = req.params;
+  let sql = `
+    SELECT page_views, clicks,  date
+    FROM users_statistic
+    INNER JOIN users ON users_statistic.user_id = users.id
+    WHERE id = ? AND date >= ? AND date <= ?
+    `;
+
+  db.all(sql, [userId, from, to], (err, row) => {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.send({ data: row });
+    }
+  });
+});
+
+app.get("/users", (req, res) => {
+  const { page = 1, amount = 10 } = req.query;
+  const startIndex = (page - 1) * amount;
+  let usersCount = null;
+  let sql1 = `
+    SELECT count(*) as count
+    FROM users
+  `;
+  let sql2 = `
+    SELECT *
+    FROM users
+    INNER JOIN users_statistic ON users_statistic.user_id = users.id
+    GROUP BY users.id
+    LIMIT ? , ?
+    `;
+
+  db.serialize(() => {
+    db.get(sql1, (err, row) => {
+      if (err) {
+        res.send(err.message);
+      } else {
+        usersCount = row.count;
+      }
+    }).all(sql2, [startIndex, amount], (err, rows) => {
+      if (err) {
+        res.send(err.message);
+      } else {
+        const last_page = Math.ceil(usersCount / amount);
+        res.send({ last_page, data: rows });
+      }
+    });
+  });
+});
+
+app.get("/users/:userId", (req, res) => {
+  const { userId } = req.params;
+  let sql = `
+    SELECT first_name, last_name
+    FROM users
+    WHERE id = ?
+  `;
+  db.get(sql, [userId], (err, row) => {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.send({ data: row });
+    }
+  });
+});
+
+app.listen(4000, () => console.log("run server on port 4000"));
---
+++ server/sqlite.js
@@ -0,0 +1,71 @@
+﻿"use strict";
+
+const fs = require("fs");
+let sqlite3 = require("sqlite3").verbose();
+
+let userSQL = `CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  gender TEXT,
+  ip_addres TEXT
+)`;
+
+let statisticSQL = `CREATE TABLE IF NOT EXISTS users_statistic ( 
+  user_id INTEGER,
+  date TEXT,
+  page_views INTEGER,
+  clicks INTEGER
+)`;
+
+// const pathToDB = "../";
+
+function createUsersTable() {
+  let usersFile = fs.readFileSync("./data/users.json");
+  let usersJSON = JSON.parse(usersFile);
+  let statisticFile = fs.readFileSync("./data/users_statistic.json");
+  let statisticJSON = JSON.parse(statisticFile);
+  let usersDB = new sqlite3.Database(pathToDB);
+
+  usersDB.serialize(() => {
+    usersDB
+      .run(userSQL)
+      .run(statisticSQL)
+      .run("delete from " + "users")
+      .run("delete from " + "users_statistic");
+
+    let insertUser = usersDB.prepare("insert into users values (?,?,?,?,?,?)");
+
+    usersJSON.forEach((user) => {
+      insertUser.run([
+        user.id,
+        user.first_name,
+        user.last_name,
+        user.email,
+        user.gender,
+        user.ip_address,
+      ]);
+    });
+
+    insertUser.finalize();
+
+    let insertStatistic = usersDB.prepare(
+      "insert into users_statistic values (?,?,?,?)"
+    );
+
+    statisticJSON.forEach((item) => {
+      insertStatistic.run([
+        item.user_id,
+        item.date,
+        item.page_views,
+        item.clicks,
+      ]);
+    });
+
+    insertStatistic.finalize();
+  });
+  usersDB.close();
+}
+
+createUsersTable();
